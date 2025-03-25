25d7ebe6d664f21fe1bf8256361a5f0ba4a7a54b
Prihasta <prihasta2004@gmail.com>
2025-03-25 13:44:01 +0700

    HomePage template
    full view of the news homepage (template)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,6 @@ import 'package:flutter_application_1/services/data.dart';
 import 'package:flutter_application_1/services/sliderData.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -15,16 +14,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Categorymodel> categories = [];
+  List<Slidermodel> sliders = [];
 
-List<Categorymodel> categories=[];
-List<Slidermodel> sliders=[];
-
-int activeIndex = 0;
-@override
+  int activeIndex = 0;
+  @override
   void initState() {
     super.initState();
-    categories= getcategories();
-    sliders= getSliders();
+    categories = getcategories();
+    sliders = getSliders();
   }
 
   @override
@@ -34,129 +32,282 @@ int activeIndex = 0;
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          Text("Wise"), 
-          Text(
-            "Indo", 
-            style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-            )
-
+            Text("Wise"),
+            Text(
+              "Indo",
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+            ),
           ],
-
         ),
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          Container(
-            margin: EdgeInsets.only(left: 10.0),
-            height: 70,
-            child: ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            itemCount: categories.length,
-            itemBuilder: (context, index) {
-              return CategoryTile(
-                image: categories[index].image, 
-                categoryName: categories[index].categoryName,
-              );
-            }),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 10.0),
+                height: 70,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return CategoryTile(
+                      image: categories[index].image,
+                      categoryName: categories[index].categoryName,
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Breaking News!",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              CarouselSlider.builder(
+                itemCount: sliders.length,
+                itemBuilder: (context, index, realIndex) {
+                  String? res = sliders[index].image;
+                  String? res1 = sliders[index].name;
+                  return buildImage(res!, index, res1!);
+                },
+                options: CarouselOptions(
+                  height: 250,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      activeIndex = index;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Center(child: buildIndicator()),
+              SizedBox(height: 30.0),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Trending News!",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 5.0,
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              "images/sports.jpg",
+                              height: 120,
+                              width: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        Column(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.7,
+                              child: Text(
+                                "Rui Costa outsprints breakaway to win stage 15",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 17.0,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 7.0),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.7,
+                              child: Text(
+                                "Then a final kick to beat lennard",
+                                style: TextStyle(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 5.0,
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              "images/sports.jpg",
+                              height: 120,
+                              width: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        Column(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.7,
+                              child: Text(
+                                "Rui Costa outsprints breakaway to win stage 15",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 17.0,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 7.0),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 1.7,
+                              child: Text(
+                                "Then a final kick to beat lennard",
+                                style: TextStyle(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 30.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Breaking News", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18.0),),
-                Text("View All", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 16.0),),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          CarouselSlider.builder(
-          itemCount: sliders.length, 
-          itemBuilder: (context, index, realIndex){
-            String? res= sliders[index].image;
-            String? res1= sliders[index].name;
-            return buildImage(res!, index, res1!);
-          }, 
-          options: CarouselOptions(
-            height: 250, 
-            autoPlay: true,
-            enlargeCenterPage: true, 
-            enlargeStrategy: CenterPageEnlargeStrategy.height,
-            onPageChanged: (index, reason){
-            
-              setState(() {
-                activeIndex= index;
-              });
-            })),
-            SizedBox(height: 30.0,),
-            Center(child: buildIndicator()),
-            SizedBox(
-              height: 30.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-              child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Breaking News", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18.0),),
-                Text("View All", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 16.0),),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
+    );
+  }
+
+  Widget buildImage(String image, int index, String name) => Container(
+    margin: EdgeInsets.symmetric(horizontal: 5.0),
+    child: Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            image,
+            height: 250,
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          height: 250,
+          padding: EdgeInsets.only(left: 10.0),
+          margin: EdgeInsets.only(top: 170.0),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Colors.black26,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+          child: Text(
+            name,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+  Widget buildIndicator() => AnimatedSmoothIndicator(
+    activeIndex: activeIndex,
+    count: sliders.length,
+    effect: ExpandingDotsEffect(
+      dotWidth: 13,
+      dotHeight: 13,
+      activeDotColor: Colors.blue,
     ),
   );
 }
-  Widget buildImage(String image, int index, String name) => Container(
-   margin: EdgeInsets.symmetric(horizontal: 5.0),
-    child: Stack(
-      children: [
-      ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Image.asset(
-          image, 
-          height: 250,
-          fit: BoxFit.cover, 
-          width: MediaQuery.of(context).size.width,
-        ),
-      ), 
-      Container(
-        height: 250,
-        padding: EdgeInsets.only(left: 10.0),
-        margin: EdgeInsets.only(top: 170.0),
-        width:  MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(color: Colors.black26, borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10) ) ),
-      child: Text(name, style: TextStyle(
-        color: Colors.white, 
-        fontSize: 18.0, 
-        fontWeight: FontWeight.bold),
-        ),
-      )
- ] ));
-      Widget buildIndicator() => AnimatedSmoothIndicator(
-        activeIndex: activeIndex, 
-        count: sliders.length,
-        effect: ExpandingDotsEffect(dotWidth: 13, dotHeight: 13, activeDotColor: Colors.blue),
-        );
-}
-
-
 
 class CategoryTile extends StatelessWidget {
   final image, categoryName;
   const CategoryTile({super.key, this.categoryName, this.image});
-
 
   @override
   Widget build(BuildContext context) {
@@ -167,25 +318,30 @@ class CategoryTile extends StatelessWidget {
           ClipRRect(
             borderRadius: BorderRadius.circular(6),
             child: Image.asset(
-              image, 
-              width: 120, 
+              image,
+              width: 120,
               height: 70,
               fit: BoxFit.cover,
             ),
           ),
           Container(
-              width: 120, 
-              height: 70,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-                color: Colors.black26,
-              ),
+            width: 120,
+            height: 70,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.black26,
+            ),
             child: Center(
               child: Text(
-                categoryName, 
-                style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-              )),
-          )
+                categoryName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
