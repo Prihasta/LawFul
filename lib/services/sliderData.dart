9bcfafec823613d14cae60ac97566c3bcb6c8ec1
Prihasta <prihasta2004@gmail.com>
2025-06-05 10:41:54 +0700

    Final
    Webscraped from JDIH

--- lib/services/sliderData.dart
+++ lib/services/sliderData.dart
@@ -1,12 +1,36 @@
 import 'package:flutter_application_1/model/sliderModel.dart';
 
-List<Slidermodel> getSliders() {
+List<SliderModel> getSliders() {
   return [
-    Slidermodel(image: "images/business.jpg", name: "Bow to the authority of silenforfce"),
-    Slidermodel(image: "images/entertainment.jpg", name: "Bow to the authority of silenforfce"),
-    Slidermodel(image: "images/health.jpg", name: "Bow to the authority of silenforfce"),
-    Slidermodel(image: "images/science.jpg", name: "Bow to the authority of silenforfce"),
-    Slidermodel(image: "images/sports.jpg", name: "Bow to the authority of silenforfce"),
-    Slidermodel(image: "images/tech.jpg", name: "Bow to the authority of silenforfce"),
+    SliderModel(
+      image: "images/business.jpg", 
+      name: "Peluncuran Smartphone Baru Membawa Inovasi Kamera 200MP",
+      description: "Perusahaan teknologi ternama resmi meluncurkan smartphone terbaru dengan kamera 200MP pertama di dunia. Fitur ini diharapkan menjadi standar baru dalam dunia fotografi mobile."
+    ),
+    SliderModel(
+      image: "images/entertainment.jpg", 
+      name: "Jalan Tol Baru Diresmikan, Waktu Tempuh Dipangkas 50%",
+      description: "Pemerintah meresmikan jalan tol penghubung kota A dan kota B yang memangkas waktu perjalanan dari 4 jam menjadi hanya 2 jam, meningkatkan efisiensi transportasi antar daerah."
+    ),
+    SliderModel(
+      image: "images/health.jpg", 
+      name: "Film Animasi Lokal Tembus Box Office Asia Tenggara",
+      description: "Film animasi produksi lokal mencetak rekor sebagai film terlaris di Asia Tenggara minggu ini, mengalahkan beberapa judul internasional populer."
+    ),
+    SliderModel(
+      image: "images/science.jpg", 
+      name: "Penemuan Obat Herbal Baru Diklaim Efektif Redakan Stres",
+      description: "Peneliti universitas menemukan kombinasi herbal yang efektif menurunkan tingkat stres tanpa efek samping, membuka jalan baru bagi pengobatan alami."
+    ),
+    SliderModel(
+      image: "images/sports.jpg", 
+      name: "Festival Budaya Kembali Digelar, Hadirkan Ratusan Penampil",
+      description: "Setelah vakum selama tiga tahun, festival budaya terbesar di Indonesia kembali digelar dengan ratusan penampil dari dalam dan luar negeri, menarik ribuan pengunjung."
+    ),
+    SliderModel(
+      image: "images/tech.jpg", 
+      name: "Game Lokal Baru Tembus 1 Juta Unduhan di Pekan Pertama",
+      description: "Sebuah game mobile buatan developer Indonesia sukses besar dengan menembus 1 juta unduhan hanya dalam 7 hari sejak peluncuran. Game bergenre petualangan ini mendapat ulasan positif karena grafik dan gameplay-nya yang menarik."
+    ),
   ];
 }
